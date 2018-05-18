a06e56dc1a98edddea6090976ef48bc3f078ac1b
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2018-05-18 12:25:56 -0700

    Edit php

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en-us">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Rizky Abraham | Portfolio</title>
+        <meta name="description" content="This is my portfolio">
+        <link rel="stylesheet" href="styles/style.css">
+        <link rel="stylesheet" href="styles/normalize.css">
+        <link href="https://fonts.googleapis.com/css?family=Cormorant" rel="stylesheet">
+    </head>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include('header.php'); ?>
+
+<body>
+    <header>
+    </header>
+
+    <nav>
+        <!--There will be a main menu over here-->
+    </nav>
+
+    <main>
+        <!--There will be a main content over here-->
+        <?php echo="Just a test";
+    </main>
+    <footer>
+        <p>&copy; 2018 &vert; Rizky Abraham &vert; rizkyabraham88@gmail.com &vert; (208) 309-0202
+            <p>
+    </footer>
+</body>
+
+</html>
